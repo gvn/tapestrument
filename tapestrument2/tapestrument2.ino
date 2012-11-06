a03cfdddd80f8f52a00954774a6e15d6e643982f
Gvn Suntop <gavin@gsuntop.com>
2012-11-05 19:58:29 -0800

    4 spaces

--- tapestrument2/tapestrument2.ino
+++ tapestrument2/tapestrument2.ino
@@ -11,32 +11,29 @@ byte noteOn = 144;
 int motorPin = 9;
 int speed = 148;
 
-void setup()
-{
-  Serial.begin(57600);
-  mySerial.begin(31250);
-  pinMode(motorPin, OUTPUT);  
+void setup() {
+    Serial.begin(57600);
+    mySerial.begin(31250);
+    pinMode(motorPin, OUTPUT);  
 }
 
-void checkMIDI(){
-  do{
-    if (mySerial.available()){
-      commandByte = mySerial.read();
-      noteByte = mySerial.read();
-      velocityByte = mySerial.read();
+void checkMIDI() {
+    do {
+        if (mySerial.available()) {
+            commandByte = mySerial.read();
+            noteByte = mySerial.read();
+            velocityByte = mySerial.read();
 
-      if (commandByte == noteOn){
-          speed = noteByte + 100;
-          Serial.println(speed);
-      }
-    }
-  }
-  while (mySerial.available() > 24);//when three bytes available
+            if (commandByte == noteOn) {
+                speed = map(noteByte, 48, 72, 148, 255);
+                Serial.println(speed);
+            }
+        }
+    } while (mySerial.available() > 24); // When three bytes are available
 }
 
-void loop()
-{
-  checkMIDI();  
-  analogWrite(motorPin, speed);  
-  delay(1); //Hold it!
+void loop() {
+    checkMIDI();
+    analogWrite(motorPin, speed);
+    delay(1);
 }
